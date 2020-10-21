1bd6d6adb3a3cf309cc042ac5184544f0da848ac
MajedAbdullah5 <44294277+MajedAbdullah5@users.noreply.github.com>
2020-10-21 22:05:42 +0600

    topBanner Added

---
+++ src/components/BannerComponent/topBanner.js
@@ -0,0 +1,29 @@
+import React, {Component, Fragment} from 'react';
+import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
+import {Container,Row,Col,Button} from "react-bootstrap";
+
+
+class TopBanner extends Component {
+    render() {
+        return (
+          <Fragment>
+              <Container className="topFixedBanner p-0" fluid={true}>
+                  <div className="topBannerOverlay">
+                      <Container className="bannerComponent">
+                        <Row>
+                            <Col className="text-center">
+                               <h1 className="bannerTitle">SOFTWARE ENGINEERING</h1>
+                                <h4 className="bannerSubTitle">Mobile & Web Application</h4>
+                                <Button varient="primary">Learn More</Button>
+                            </Col>
+                        </Row>
+                      </Container>
+                  </div>
+              </Container>
+          </Fragment>
+        );
+    }
+}
+
+export default TopBanner;
---
+++ src/components/Services/services.js
@@ -0,0 +1,26 @@
+import React, {Component, Fragment} from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+
+class Services extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1>1</h1>
+                        </Col>
+                        <Col>
+                            <h1>2</h1>
+                        </Col>
+                        <Col>
+                            <h1>3</h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default Services;
